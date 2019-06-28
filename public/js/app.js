07eaefddd81ff025e8940cea2b1278bb90143d52
christiantalmadge <christiantalmadge@gmail.com>
2019-06-28 14:54:22 -0700

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,35 @@
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
+weatherForm.addEventListener('submit', (e) =>
+{
+    //prevent browser refresh
+    e.preventDefault()
+    //extract input
+    const location = search.value
+
+    messageOne.textContent = 'Loading weather data'
+    messageTwo.textContent = ''
+    //Input is added to the dynamic url to fetch data for the corresponding location
+    //Kicks off asynchronous io operation
+    fetch('http://localhost:3000/weather?address='+ location).then((response) => {
+
+        //runs once json data has arrived
+        response.json().then((data) =>
+        {
+            if(data.error)
+            {
+                messageOne.textContent = data.error
+            }
+            else
+            {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,23 @@
+const request = require('request')
+
+const forecast = (lat, lng, callback) => {
+
+    const url = 'https://api.darksky.net/forecast/ca8742a2c3a947c73301da318ed5fb97/' + lat +',' + lng
+    request({url, json: true}, (error, {body})=> {
+    if(error)
+    {
+        callback('Unable to connect to weather service!', undefined)
+    }
+    else if(body.error)
+    {
+        callback('Unable to find location', undefined)
+    }
+    else
+    {
+        callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out.' + ' There is a ' + body.currently.precipProbability + '% chance of rain.')
+    }
+
+})
+
+}
+module.exports = forecast
